937afc977c4017cdc53cf3bf3d93e64822319a04
Fred Frey <f.frey6@gmail.com>
2018-05-03 13:24:40 -0400

    pushing for walter.

--- Rover/Rover.ino
+++ Rover/Rover.ino
@@ -1,17 +1,32 @@
-#include <newPing.h>
+#include <FastPID.h>
+
+#include <NewPing.h>
+
 #include <IRremote.h>
+#include <ir_Lego_PF_BitStreamEncoder.h>
+#include <IRremoteInt.h>
+#include <boarddefs.h>
 
 #define PWM_PINA 5
 #define PWM_PINB 6
 #define DIR_A 4
 #define DIR_B 7
 #define TRIG_PIN 11
+#define MAX_DISTANCE 200
 #define ECHO_PIN0 12
 
 enum {MOTORB, MOTORA};
 
 int speedA, speedB = 0;
 int directionA, directionB = HIGH;
+
+float Kp=0.1, Ki=0.5, Kd=0;
+int output_bits = sizeof ((unsigned long) 0);
+bool output_signed = false;
+
+FastPID forward_pid(Kp, Ki, Kd, output_bits, output_signed);
+
+NewPing forward_sonar(TRIG_PIN, ECHO_PIN0, MAX_DISTANCE);
 
 void setup() {
   // put your setup code here, to run once
@@ -22,46 +37,44 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN0, INPUT);
 
+  Serial.begin(9600);
+
   pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //setmotor(0,0,MOTORA);
-  forward(200);
-  delay(1000);
-  //digitalWrite(13, LOW);
-  //turnRight();
-  delay(1000);
-  //digitalWrite(13, HIGH);
-  //turnRight();
-  //delay(1000);
-  //digitalWrite(13, LOW);
-  //reverse(200);
-  //delay(1000);
-  //digitalWrite(13, HIGH);
-  forward(0);
-  while (1);
+  unsigned long dist = forward_sonar.ping_cm();
+  Serial.println(dist);
+  if (dist < 20 && dist!=0){/*
+      unsigned long setpoint = 20; 
+      unsigned long feedback = dist;
+      forward(forward_pid.step(setpoint, feedback));*/
+      forward(0);
+    }
+  else forward(100);
+  delay(50);
 }
 
 void setmotor(int speed, int direction, int motor) {
   speed = direction?255-speed:speed;
   int pwm_pin = motor?PWM_PINA:PWM_PINB;
-  int dir_pin = motor?DIR_B:DIR_A;
+  int dir_pin = motor?DIR_A:DIR_B;
   motor?speedA=speed:speedB=speed; 
   motor?directionA=direction:directionB=direction; 
   analogWrite(pwm_pin, speed);
   digitalWrite(dir_pin, direction);
 }
 
-void turnRight() {
-  setmotor(200, 1, MOTORB);
-  setmotor(200, 0, MOTORA);
+void turnRight(int speed) {
+  setmotor(speed, 1, MOTORB);
+  setmotor(speed, 0, MOTORA);
 }
 
-void turnLeft() {
-  setmotor(200, 0, MOTORB);
-  setmotor(200, 1, MOTORA);
+void turnLeft(int speed) {
+  setmotor(speed, 0, MOTORB);
+  setmotor(speed, 1, MOTORA);
 }
 
 void forward(int speed) {
